c8030abb4e26fd730524f47b016cfc722ed3ecff
Chris Plock <plockc2@icloud.com>
2022-10-11 20:57:35 -0700

    add README and missing Memory storage

---
+++ nbd/README.md
@@ -0,0 +1,7 @@
+Some references
+
+busybox implementation [src](https://git.busybox.net/busybox/tree/networking/nbd-client.c)
+
+[NBD Protocol](https://github.com/NetworkBlockDevice/nbd/blob/master/doc/proto.md), and implementations of client and server are there as well
+
+[buse-go](https://github.com/samalba/buse-go)
---
+++ nbd/memory_device.go
@@ -0,0 +1,55 @@
+package nbd
+
+import (
+	"fmt"
+	"log"
+)
+
+var _ Storage = &Memory{}
+
+type Memory struct {
+	data []byte
+}
+
+func NewMemory() Storage {
+	m := Memory{}
+	m.data = make([]byte, m.Size())
+	return &m
+}
+
+func (m *Memory) ReadAt(p []byte, off uint64) error {
+	if int(off)+len(p) > len(m.data) {
+		return fmt.Errorf(
+			"cannot read %d bytes starting at %d with disk size %d",
+			len(p), off, len(m.data),
+		)
+	}
+	copy(p, m.data[off:int(off)+len(p)])
+	log.Printf("MEMORY READ at:%d, %d bytes\n", off, len(p))
+	return nil
+}
+
+func (m *Memory) WriteAt(p []byte, off uint64) error {
+	if int(off)+len(p) > len(m.data) {
+		return fmt.Errorf(
+			"cannot write %d bytes starting at %d with disk size %d",
+			len(p), off, len(m.data),
+		)
+	}
+	copy(m.data[off:], p)
+	log.Printf("MEMORY WRITE at:%d, %d bytes\n", off, len(p))
+	return nil
+}
+
+func (m *Memory) TrimAt(off uint64, length uint32) error {
+	log.Printf("MEMORY TRIM at:%d, %d bytes\n", off, length)
+	return nil
+}
+
+func (m *Memory) Size() uint64 {
+	return 1 * 1024 * 1024
+}
+
+func (m *Memory) Disconnect() {
+	log.Println("MEMORY DISCONNECT")
+}
